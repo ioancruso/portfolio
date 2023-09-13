2873b034a93d0a95a00187e413451dc2921408cb
Crușoveanu Ioan <ionut.cruso@gmail.com>
2023-09-13 22:06:01 +0300

    - added one new project

--- pages/projects.js
+++ pages/projects.js
@@ -1,22 +1,23 @@
-import { ProjectCard } from "../components/content/ProjectCard"
-import Head from 'next/head'
+import {ProjectCard} from "../components/content/ProjectCard";
+import Head from "next/head";
 
 import styles from "../styles/content/components/projects.module.scss";
 
-import data from '../data/projects.json';
+import data from "../data/projects.json";
 
 function Projects() {
-
-    return <>
-        <Head>
-            <title>My projects</title>
-        </Head>
-        <div className={styles.projectsSection}>
-        {data.map((project) => (
-                <ProjectCard key={project.id} project={project}/>
-        ))}
-        </div>
-    </>
+    return (
+        <>
+            <Head>
+                <title>My projects</title>
+            </Head>
+            <div className={styles.projectsSection}>
+                {[...data].reverse().map((project) => (
+                    <ProjectCard key={project.id} project={project} />
+                ))}
+            </div>
+        </>
+    );
 }
-  
-export default Projects
+
+export default Projects;
