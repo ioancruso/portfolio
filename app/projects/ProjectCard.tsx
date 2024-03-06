76f55b059237a0a0ec89818b47336bd01dfcc16a
Crusoveanu Ioan <ionut.cruso@gmail.com>
2024-03-06 20:08:14 +0200

    - changed the layout of the project page - changed the buttons for the project card - changed the colors for buttons - remove iframe - added simple screenshots instead of the iframes - changed the descriptions of the projects

--- app/projects/ProjectCard.tsx
+++ app/projects/ProjectCard.tsx
@@ -1,88 +1,67 @@
 "use client";
 
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./projectcard.module.scss";
 
-import Technologies from "@/svgs/technologies";
 import Demo from "@/svgs/demo";
 import Github from "@/svgs/github2";
 
 interface Project {
-    name: string;
-    description: string;
-    preview: string;
-    about: {
-        demo: string;
-        repository?: string;
-    };
+	name: string;
+	description: string;
+	type: string;
+	about: {
+		demo: string;
+		repository?: string;
+		screenshot: string;
+	};
 }
 
-interface ProjectCardProps {
-    project: Project;
+function ProjectCard({ project }: { project: Project }) {
+	const { name, description, type, about } = project;
+
+	return (
+		<>
+			<div className={styles.projectCard}>
+				<div className={styles.title}>{name}</div>
+				<div className={styles.type}>- {type} -</div>
+				<div className={styles.imageContainer}>
+					<img
+						src={`projects/${about.screenshot}`}
+						className={styles.image}
+					/>
+				</div>
+				<div className={styles.details}>
+					<a
+						href={about.demo}
+						target="_blank"
+						className={styles.button}
+						rel="noopener noreferrer nofollow"
+					>
+						<Demo width={32} height={32} />
+						<div>Demo</div>
+					</a>
+					{about.repository && (
+						<a
+							href={about.repository}
+							target="_blank"
+							className={styles.button}
+							rel="noopener noreferrer nofollow"
+						>
+							<Github width={32} height={32} />
+							<div>Repository</div>
+						</a>
+					)}
+				</div>
+				<div className={styles.about}>
+					{description.split("///").map((item: string, index: number) => (
+						<li key={index}>{item}</li>
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
 
-function ProjectCard(props: ProjectCardProps) {
-    const project = props.project;
-    const {name, description, preview, about} = project;
-    const [iframeLoaded, setIframeLoaded] = useState(false);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setIframeLoaded(true);
-        }, 300); // Set the iframeLoaded state to true after 2 seconds
-
-        return () => clearTimeout(timer); // Clear the timeout if the component is unmounted before 2 seconds
-    }, []);
-
-    return (
-        <>
-            <div className={styles.projectCard}>
-                <div className={styles.title}>{name}</div>
-                <div className={styles.presentation}>
-                    <iframe
-                        src={preview}
-                        className={`${styles.iframe} ${
-                            iframeLoaded
-                                ? styles.iframeVisible
-                                : styles.iframeHidden
-                        }`}
-                        title="Embedded Site"
-                        scrolling="no"
-                    ></iframe>
-                    <div className={styles.details}>
-                        <a
-                            href={about.demo}
-                            target="_blank"
-                            className={`${styles.category} ${styles.link}`}
-                            rel="noopener noreferrer nofollow"
-                        >
-                            <Demo width={50} height={50} />
-                            <div>Demo</div>
-                        </a>
-                        {about.repository && (
-                            <a
-                                href={about.repository}
-                                target="_blank"
-                                className={`${styles.category} ${styles.link}`}
-                                rel="noopener noreferrer nofollow"
-                            >
-                                <Github width={50} height={50} />
-                                <div>Repository</div>
-                            </a>
-                        )}
-                    </div>
-                </div>
-                <div className={styles.about}>
-                    {description
-                        .split("/")
-                        .map((item: string, index: number) => (
-                            <li key={index}>{item}</li>
-                        ))}
-                </div>
-            </div>
-        </>
-    );
-}
-
-export {ProjectCard};
+export { ProjectCard };
