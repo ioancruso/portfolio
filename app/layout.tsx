0d2718042a09989d4c469434c71bd0ad39a09c08
Ioan <ionut.cruso@gmail.com>
2024-12-09 01:28:27 +0200

    - added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { cookies } from "next/headers";
 
+import { Analytics } from "@vercel/analytics/next";
 import { AppSidebar } from "@/components/app-sidebar";
 import {
 	SidebarProvider,
@@ -60,6 +61,7 @@ export default function RootLayout({
 						</main>
 					</SidebarInset>
 				</SidebarProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
